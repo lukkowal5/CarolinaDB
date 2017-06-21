3ce013bfcde4ceba258bb130f92bfaf15a0c0855
lukkowal5 <lukkowal5@gmail.com>
2017-06-22 00:56:57 +0200

    Initial commit

---
+++ src/main/java/com/pimeyes/carolinaDB/CarolinaDbApplication.java
@@ -0,0 +1,12 @@
+package com.pimeyes.carolinaDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarolinaDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarolinaDbApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pimeyes/carolinaDB/CarolinaDbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pimeyes.carolinaDB;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarolinaDbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
